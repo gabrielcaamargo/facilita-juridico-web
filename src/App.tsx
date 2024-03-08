f39174753dedf406a163fb04df145d468fc364ce
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-03-07 21:13:47 -0300

    feat: create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './components/Router';
+import { Header } from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <AppRoutes />
     </BrowserRouter>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="h-16 bg-[#376fd0] flex items-center justify-center w-full">
+      <strong className="text-white text-xl md:text-2xl">Facilita Jurídico</strong>
+    </header>
+  );
+}
