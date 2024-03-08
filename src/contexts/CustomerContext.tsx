c55ca9db98da71c9799a23d033e100e59a7bddb2
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-03-07 23:57:49 -0300

    feat: create customer context

---
+++ src/contexts/CustomerContext.tsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from 'react';
+import { ICustomer } from '../@types/ICustomer';
+
+interface ICustomerContext {
+  customers: ICustomer[],
+  setCustomers: (setState: ICustomer[]) => void
+}
+
+interface ICustomerContextProps {
+  children: React.ReactNode
+}
+
+const CustomerContext = createContext({} as ICustomerContext);
+
+function CustomerContextProvider({ children }: ICustomerContextProps) {
+  const [customers, setCustomers] = useState([] as ICustomer[]);
+
+  return (
+    <CustomerContext.Provider value={{ customers, setCustomers }}>
+      {children}
+    </CustomerContext.Provider>
+  );
+}
+
+export { CustomerContextProvider };
--- src/utils/HttpClient.ts
+++ src/utils/HttpClient.ts
@@ -12,5 +12,4 @@ class HttpClient {
   }
 }
 
-
 export { HttpClient };
